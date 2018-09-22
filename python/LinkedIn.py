ccebfe67e10c40adce362a014c08771b2a6f846c
cristianmaral <cristianmaralsilva@gmail.com>
2018-09-22 18:58:44 -0300

    Inclusão de filtros específicos e otimização do código

--- python/LinkedIn.py
+++ python/LinkedIn.py
@@ -10,33 +10,36 @@ class LinkedIn:
         self.access_token = access_token
 
     def make_request(self, url, params=None):
+        print url
         if (params == None):
             params = {}
         params.update({'format':'json'})
         params.update({'oauth2_access_token': self.access_token})
         return requests.get(url, params).json()
 
-    def get_profile_id(self, id=None):
+    def get_profile_id(self, id=None, fields=None):
         if id:
             url = '%s/id=%s' % (self.PROFILE, str(id))
         else:
             url = '%s/~' % (self.PROFILE)
+        if fields:
+            url = '%s:(%s)' % (url, ','.join(fields))
         return self.make_request(url)
 
-    def get_profile_url(self, url):
+    def get_profile_url(self, url, fields=None):
         url = '%s/url=%s' % (self.PROFILE, quote_plus(url))
+        if fields:
+            url = '%s:(%s)' % (url, ','.join(fields))
         return self.make_request(url)
 
-    def get_companies_ids(self, ids):
-        identifiers = []
-        identifiers += map(str, ids)
-        url = '%s::(%s)' % (self.COMPANY, ','.join(identifiers))
+    def get_company_id(self, id, fields=None):
+        url = '%s/%s' % (self.COMPANY, str(id))
+        if fields:
+            url = '%s:(%s)' % (url, ','.join(fields))
         return self.make_request(url)
 
-    def get_companies_universal_names(self, universal_names):
-        identifiers = []
-        identifiers += ['universal-name=%s' % un for un in universal_names]
-        url = '%s::(%s)' % (self.COMPANY, ','.join(identifiers))
+    def get_company_universal_name(self, universal_name):
+        url = '%s/universal-name=%s' % (self.COMPANY, universal_name)
         return self.make_request(url)
 
     def get_company_updates(self, id, count):
